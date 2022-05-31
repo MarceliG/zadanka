5ed07803184703ba3a1c06620cd5ce43013e590e
MarceliG <marcel9817@gmail.com>
2022-05-31 20:51:15 +0200

    deaf_rats

--- deaf_rats.py
+++ deaf_rats.py
@@ -1,37 +1,41 @@
 
 def count_deaf_rats(town):
-    town_list = list(town)
+    town_list = [element for element in list(town) if element != ' ']
+
     piper = False
     tw = ''
     deaf_rat = 0
     for element in town_list:
         tw += element
-        if tw == "~O" and piper == False:
+        if element == 'P':
+            piper = True
             tw = ''
-            print(True)
-        elif tw == "O~" and piper == False:
+        elif tw == "O~" and piper == False: # piper is on the right
+            tw = ''
+            deaf_rat += 1
+        elif tw == "~O" and piper == True: # piper is on the left
             tw = ''
             deaf_rat +=1
-            
-        # if element == "~" and element.index()+1 ==  
-        # if element == 'P':
-        #     piper = True
-    print(deaf_rat)
-    return tw
+        elif tw == "O~" or tw == "~O":
+            tw = ''
+        
+       
+    return deaf_rat
 
 
     # return town
 
-
+def how_should_do(town):
+    return town.replace(" ","")[0::2].count("O")
 
 
 rats0 = "~O~O~O~O P" # 0
 rats1 = "P O~ O~ ~O O~" # 1
 rats2 = "~O~O~O~OP~O~OO~" # 2
-# print(name)
 
-print(count_deaf_rats(rats0))
 
+print(count_deaf_rats(rats2))
+print(how_should_do(rats2))
 
 
 
